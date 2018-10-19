353431aa667ec1b7ab4aee35def00d4d490d2575
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-19 09:47:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/jokenpokemon/Tela1Activity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.jokenpokemon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+    }
+}
